1a32d88a77cb862fbbd8492d05755589bf3ca1b4
honyava <88489271+honyava@users.noreply.github.com>
2023-04-20 14:24:59 +0300

    123
    123

---
+++ SendDeviation.m
@@ -0,0 +1,7 @@
+function SendDeviation(deviation, RAMP)
+    Ampl_u = round(deviation/620.424); % deviatin - kHz;  Ampl_u - mV.
+    Ampl_u = round(Ampl_u/0.8059);
+%     command = uint32(0);
+    command = uint32(7) + bitshift(uint32(RAMP), 8) + uint32(bitshift(uint32(Ampl_u), 16));
+    s.write(command,"uint32");
+end
