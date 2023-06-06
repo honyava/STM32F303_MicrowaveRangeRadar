4ed152c5fbdb537e9cfb8203611b1606e4292e97
honyava <88489271+honyava@users.noreply.github.com>
2023-06-06 11:56:25 +0300

    ALL
    ALL

--- UART.m
+++ UART.m
@@ -1,30 +1,53 @@
-clear;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% В данном ПО представлен интерфейс для работы с радиодальномером (FM24).
+% Функция SendDeviation отвечает за преобразование желаемой пользователем
+% девиацией (измеряется в кГц) в напряжение (мВ) для заданной формы пилы 
+% (RAMP1 - симметричная, RAMP2 - несимметричная).
+% Пример: SendDeviation(500e3, coeff, RAMP1,  s);  (500e3 = 500МГц)
+% coeff - задает чувствительности настройки VCO (задает пользователь во 
+% время выполнения программы).
+% Функция SendCommand может отправлять команды:  
+% 1) Остановка ADC (STOP) 
+% 2) Рестар контроллера (RESET)
+% 3) Отпарвка и получение тестовой команды, т.е. эхо (TEST)
+% 4) Задание формы пилы (RAMP1 или RAMP2).
+% Пример: SendCommand(RESET, s).
+%
+% Перед началом работы пользователь должен определить параметры:
+% 1) period_Number_stm32 - число периодов DAC, которое подается на stm32.
+% Возможный диапазон от 1 до 4 включительно.
+% 2) period_Number_pc - сколько раз ПК будет собирать данные размером 
+% period_Number_stm32. Минимум значнте должно быть 1, 
+% по максимуму неограничена.
+% 3) SIZE_ADC - Размер буффера ADC. Должен совпадать с параметром на stm32
+% 4) ADC_PER_DAC - Число периодов сбора данных, приходящихся на 1 период
+% DAC. Должно совпадать с параметром на stm32.
+% 5) ADC_FREQ - частота сбора данных ADC. Должна совпадать с параметром на 
+% stm32.
+% 
+% На графиках будет отображаться period_Number_stm32*period_Number_pc
+% периодов сигнала.
+% Все комманды отправляются от 1 до 4 байт включительно.
+% Ниже приведена таблица соответсвия по командам.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % bytes   [0]          [1]            [2]          [3]
-% START    1    period_Number_stm32<=14      -            -    
+% START    1   period_Number_stm32<=14 -            -    
 % STOP     2            -              -            -
 % RESET    3            -              -            -
 % TEST     4            -              -            -
 % RAMP1    5            -              -            -
 % RAMP2    6            -              -            -
-% AMPL     7          5 or 6            Amplitude(0 - 4095) - 12bite ()
+% AMPL     7      RAMP1 or RAMP2    Amplitude(0 - 4095) - 12bite ()
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+clear;
 START = 1; STOP = 2; RESET = 3; TEST = 4; RAMP1 = 5; RAMP2 = 6; AMPL = 7;
-
-%SendDeviation(500e3, coeff, RAMP1,  s); %deviation - kHz (in function 500e3 = 500MHz)
-%SendCommand(command, s) %only for STOP_RESET_TEST_RAMP1_RAMP2
-
-period_Number_stm32 = 4;  % max period_Number_stm32_stm32 = 4
-period_Number_pc = 1; % can be any number
+period_Number_stm32 = 4;
+period_Number_pc = 1;
 SIZE_ADC = 128;
-ADC_PER_DAC = 4; % amount perios of ADC in 1 period DAC
+ADC_PER_DAC = 4;
 ADC_FREQ = 576e3;
-
-% on graph will be period_Number_stm32*period_Number_pc PERIODS of signal
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 s = serialport("COM6", 3e6, 'Timeout', 1);
 pause(1);
@@ -42,7 +65,7 @@ while(true)
         SendStart(period_Number_stm32, s);
         size_m = period_Number_stm32*SIZE_ADC*ADC_PER_DAC + 1;
         data = uint32([]); % Создаем пустой массив для данных
-        message_size = (size_m - 1)*size_uint32; % in bytes
+        message_size = (size_m - 1)*size_uint32; % размер в байтах
         preamble = bitshift(uint32(1), 0) + uint32(bitshift(uint32(period_Number_stm32), 8)) + uint32(bitshift(uint32(message_size), 16));
         while(length(data) < size_m) 
             new_data = uint32(read(s, size_m - length(data), 'uint32'));
@@ -90,7 +113,6 @@ while(true)
             ylabel("|P1(f)|")
             xlim([0, 20*10^3]) % Ограничение оси x до 20*10^3
             grid on;
-            % drawnow;
 
             subplot(4, 1, 3) % Создание третьего графика
             plot(t,All_Channel_new(2,:)) % График отсчетов Channel1
